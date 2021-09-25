f0300dc5c87e22fcf1a73192c103dde37987c590
Skylar Kantor <skylkantor@gmail.com>
2021-09-25 10:24:21 -0400

    fixed comments

--- Project 1/CSE321_project1_alexkant_corrected_code.cpp
+++ Project 1/CSE321_project1_alexkant_corrected_code.cpp
@@ -22,35 +22,40 @@ int main() {
     printf("Starting state of thread: %d\n", controller.get_state());
     controller.start(TimedLEDToggle);
     printf("State of thread right after start: %d\n", controller.get_state());
-    //On the rising edge (when pressed), call HoldingButton
+    //On the rising edge (when pressed), set a flag that indicates the button is down
     Button1.rise(HoldingButton);
-    //On the falling edge (release), call CountPresses
+    //On the falling edge (release), determine whether the next press will be odd or even
     Button1.fall(CountPresses);
     return 0;
 }
 
+//make the handler
 void TimedLEDToggle() {
     while (true) {
         //Only do this every other press, starting with the first
         if(oddPress == 0){
             BlueLED = !BlueLED;
-            printf("Toggled Blue LED on");
-            thread_sleep_for(2000);
+            printf("Toggled Blue LED on"); 
+            thread_sleep_for(2000); //Thread_sleep is a time delay function, causes a 2000 unit delay
             BlueLED = !BlueLED;
             printf("Toggled Blue LED off"); 
-            thread_sleep_for(500); 
+            thread_sleep_for(500); //Thread_sleep is a time delay function, causes a 500 unit delay
         }
     }
 }
 
 void HoldingButton() {
+    // togle the state of the thread
 	buttonHeld = 1;
 }
 
 void CountPresses() {
     if (buttonHeld == 1){
+        //determine whether the next press will be odd (1) or even (0)
         oddPress++; 
         oddPress %= 2;
+
+        //reset the button flag for the next press
         buttonHeld = 0;
     }
 }
