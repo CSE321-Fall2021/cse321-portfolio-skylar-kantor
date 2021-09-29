0ba891d37a67fc68193898e22da1d6877c022d16
Skylar Kantor <skylkantor@gmail.com>
2021-09-29 19:01:35 -0400

    added template

---
+++ Project 1/CSE321_project1_alexkant_template.cpp
@@ -0,0 +1,26 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+/**
+*File: main.cpp
+*Author Skylar Kantor (alexkant@buffalo.edu)
+*Date: Fall 2021
+*Partner: 
+*Course: CSE 321
+*Assignent: Project 1
+*Inputs:
+*Outputs:
+*Constraints
+*Modules/Subroutines:
+*Sources:
+*File Summary: Code and header template
+*/
+
+
+int main()
+{
+    
+}
