aa2ae456d25cd5564e2930674919abd9f9d993a5
Skylar Kantor <skylkantor@gmail.com>
2021-11-05 08:28:54 -0400

    final code and README commit for part 2

--- Project 2/README.md
+++ Project 2/README.md
@@ -10,7 +10,7 @@ Skylar Kantor (alexkant@buffalo.edu)
 --------------------
 Features
 --------------------
--Counts down from any time less than or equal to 9min 59sec
+-Counts up or down from any time less than or equal to 9min 59sec
 -Runs indefinitely
 -User input for time
 --------------------
@@ -46,7 +46,23 @@ InterruptIn twoToZero: Interrupt object for the second collumn
 InterruptIn threeToNine: Interrupt object for the third collumn
 InterruptIn AToD: Interrupt object for the fourth collumn
 
-int row: Used to keep track of which row of the keypad is being powered, since each collumn shares a GPIO pin
+int row: Keeps track of which row of the keypad is being powered, since each collumn shares a pin
+
+int totalTime: The total time being counted up to or down from
+int timePos: Which digit of the time is being altered. Used to know which digit variable to change, and how much to change total time by
+int newTime: The digit pressed by the user
+
+
+int firstDigit: The digit in the minutes place on the LCD
+int secondDigit:The digit in the ten seconds place on the LCD
+int thirdDigit: The digit in the seconds place on the LCD
+
+bool enteringTime: Determines whether or not the user is entering a new time
+bool timeRunning: Determines whether or not the timer is counting
+
+char remainingTime[4]: String representation of remaining time in m:ss format
+
+bool countingDown: Determines the counting direction of the timer
 
 CSE321_LCD display: The LCD object
 
@@ -59,11 +75,11 @@ InterruptIn: creates a reference to an interrupt with a variable name, and is tr
 
   .rise: trigger when the button is depressed
 
-  .fall: trigger when the button is released
-
 wait_us: wait a certain nuber of microseconds
 
 thread_sleep_for: wait a certain number of milliseconds
+Ticker: Creates an object to call a function periodically after a certain time
+EventQueue: Provides a safe way to call non interrupt-safe functions from an interrupt context
 
 
 ----------
@@ -73,5 +89,10 @@ void one(void): Handle keypresses in the first collumn of the keypad
 void two(void): Handle keypresses in the second collumn of the keypad
 void three(void): Handle keypresses in the third collumn of the keypad
 void A(void): Handle keypresses in the fourth collumn of the keypad
-void LEDFlash(void): Blink an LED connected to PD9 
+
+void timing(void): Function that is called through the Ticker. Detects whether or not the total time has been reached, and displays the appropriate message on the LCD, and calls the apropriate helper function to display the remaining time on the LCD
+void displayTime(void): Takes the integer representation of each digit and creates a string to display on the LCD
+void timeEnter(void): Prints the time entry propmpt to the LCD
+void KeypressHandler(int col, int row): Takes a row and collumn from each collumn handler ISR and calls the apropriate helper functions and updates global variables accordingly
+void UpdateDigits (void): Changes the three integers for the display representation of the remaining time up or down depending on the counting direction
 
--- Project 2/cse321_alexkant_project2_main.cpp
+++ Project 2/cse321_alexkant_project2_main.cpp
@@ -12,33 +12,32 @@
  */
 
 #include "1802.h"
-#include "DigitalOut.h"
 #include "InterruptIn.h"
-#include "PinNamesTypes.h"
 #include "mbed.h"
 #include "mbed_events.h"
-#include "mbed_wait_api.h"
 
-// Interrup handlers     to check which key on a collumn has been pressed and react
+// Interrupt handlers to check which key on a collumn has been pressed and react
 // appropriately
 void one(void);
 void two(void);
 void three(void);
 void A(void);
 
+//Helper functions for updating the time on the display
 void timing(void);
 void displayTime(void);
+void UpdateDigits (void);
 
-// interrupt Objects for each collumn
-InterruptIn oneToSeven(PA_6, PullDown);   //D12, PA_6
-InterruptIn twoToZero(PA_5, PullDown);    //D13, PA_5
-InterruptIn threeToNine(PB_9, PullDown); //D14, PB_9
-InterruptIn AToD(PB_8, PullDown);        //D15, PB_8
+// interrupt objects for each collumn
+InterruptIn oneToSeven(PA_6, PullDown);  // D12, Pin 5
+InterruptIn twoToZero(PA_5, PullDown);   // D13, Pin 6
+InterruptIn threeToNine(PB_9, PullDown); // D14 Pin 7
+InterruptIn AToD(PB_8, PullDown);        // D15, Pin 8
 
 // EventQueue so we can work with non interrupt safe functions
 EventQueue q1(32 * EVENTS_EVENT_SIZE);
 
-Ticker tick; //Ticker for timing so we can trigger an event every second
+Ticker tick; // Ticker for timing so we can trigger an event every second
 
 int row = 0;
 
@@ -46,6 +45,7 @@ int totalTime = 0;
 int timePos = 0;
 int newTime = 0;
 
+// Integer values of each digit for string representation
 int firstDigit = 0;
 int secondDigit = 0;
 int thirdDigit = 0;
@@ -53,7 +53,10 @@ int thirdDigit = 0;
 bool enteringTime = false;
 bool timeRunning = false;
 
-char remainingTime[4];
+char remainingTime[4]; // String representation of remaining time in m:ss format
+
+bool countingDown = true; // For the bonus, make sure to detect which direction
+                          // the user wants to count
 
 CSE321_LCD display(16, 2, LCD_5x10DOTS, PF_0, PF_1);
 
@@ -77,25 +80,26 @@ int main() {
   GPIOD->MODER |= (0x1 << 18); // set pin D9(Nucleo D0, red LED) as output
 
   display.begin(); // start the LCD
+  display.print("Timer On. Press D to enter time");
 
   // setup power supply for keypad row lines
   // Row 0, 1-A
-  // Set pin F14 (Nucleo D4) as an output
+  // Set pin F14 (Nucleo D4) as an output (Pin 1 on keypad)
   GPIOF->MODER &= ~(0x2 << 28);
   GPIOF->MODER |= (0x1 << 28);
 
   // Row 1, 4-B
-  // Set pin E11 (Nucleo D5) as an output
+  // Set pin E11 (Nucleo D5) as an output (Pin 2 on keypad)
   GPIOE->MODER &= ~(0x2 << 22);
   GPIOE->MODER |= ~(0x1 << 22);
 
   // Row 2, 7-C
-  // Set pin E9 (Nucleo D6) as an output
+  // Set pin E9 (Nucleo D6) as an output (Pin 3 on keypad)
   GPIOE->MODER &= ~(0x2 << 18);
   GPIOE->MODER |= (0x1 << 18);
 
   // Row 3 *-D
-  // Set pin F13 (Nucleo D7) as an output
+  // Set pin F13 (Nucleo D7) as an output (Pin 4 on keypad)
   GPIOF->MODER &= ~(0x2 << 26);
   GPIOF->MODER |= (0x1 << 26);
 
@@ -113,8 +117,8 @@ int main() {
     // turn off row 0 (F14)
     GPIOF->ODR &= ~(0x1 << 14);
     // turn on row 1 (E11)
-    GPIOE->ODR |= (0x1 <<11);
-    wait_us(1000);
+    GPIOE->ODR |= (0x1 << 11);
+    wait_us(500);
 
     // set row to 2
     row = 2;
@@ -135,61 +139,85 @@ int main() {
     if (enteringTime) {
       // So that the user can see what time they've entered, display it here to
       // them as they type
-      display.setCursor(0, 1);
+      display.setCursor(0, 1); // Set the cursor to the second line because
+                               // we'll have some text on the first line
       displayTime();
     }
     q1.dispatch_once(); // Dispatch any items in the eventQueue
   }
 }
 
-void displayRunningTime() {
-  if (thirdDigit > 0)
-    thirdDigit--; // If there's a digit in the seconds place, subtract one from
-                  // that
-  else if (secondDigit > 0) {
-    thirdDigit = 9;
-    secondDigit--; // Otherwise, carry 10 seconds from the 10-second place and
-                   // subtract 1
+void UpdateDigits() {
+  if (countingDown) {
+    if (thirdDigit > 0)
+      thirdDigit--; // If there's a digit in the seconds place, subtract one
+                    // from that
+    else if (secondDigit > 0) {
+      thirdDigit = 9;
+      secondDigit--; // Otherwise, carry 10 seconds from the 10-second place and
+                     // subtract 1
+    } else {
+      firstDigit--;
+      secondDigit = 5;
+      thirdDigit =
+          9; // If the only digit is in the minutes place, subtract one
+             // there, carry that over to the 10-seconds place, and then
+             // carry 10 seconds to the seconds place, and subtract one
+    }
   } else {
-    firstDigit--;
-    secondDigit = 5;
-    thirdDigit = 9; // If the only digit is in the minutes place, subtract one
-                    // there, carry that over to the 10-seconds place, and then
-                    // carry 10 seconds to the seconds place, and subtract one
+    if (thirdDigit < 9)
+      thirdDigit++; //Since after 9 seconds we add to the ten seconds place, only increase until the third digit is 9
+    else if (secondDigit < 5) {
+      thirdDigit = 0; //Then, after the thrid digit is 9, set it back to 0, (ex 1:09 --> 1:10)
+      secondDigit++; //And add to the tens place, until the digit in the ten seconds place is 5. 
+                    //When there is a 9 in the seconds place, and a 5 in the 10 seconds place (ex 1:59), then
+    } else {
+      thirdDigit = 0; //Set the seconds and ten seconds places to 0
+      secondDigit = 0;
+      firstDigit++; //And add one to the minutes place (ex 1:59 --> 2:00)
+    }
   }
-  display.setCursor(0, 1);
+  display.setCursor(0, 1); // Set the cursor to the second line because we'll
+                           // have some text on the first line
   displayTime(); // print out the string representation of
                  // the 3 digits of time we have
 }
 
 void timing(void) {
   if (!timeRunning)
-    return;
+    return; // If we're not displaying the time to the user, this function
+            // should not run because other information is on-screen
   display.clear();
-  display.setCursor(0, 0);
+  display.setCursor(0,
+                    0); // Set the cursor to the top so the text prints properly
   if (totalTime > 0) {
-    display.print("Time Remaining:");
+    if (countingDown)
+      display.print("Time Remaining:");
+    else
+      display.print("Time Elapsed");
     totalTime--;
   } else {
-    display.print("Time's Up");
+    if (countingDown)
+      display.print("Time's Up");
+    else
+      display.print("Time Reached");
     GPIOD->ODR |= 0x1 << 8; // Turn on D8, the white LED
     GPIOD->ODR |= 0x1 << 9; // Turn on D9, the red LED
     return;
   }
-  displayRunningTime();
+  UpdateDigits();
 }
 
 void timeEnter() {
-  display.setCursor(0, 0);
-  display.clear();
+  display.setCursor(
+      0, 0);       // Set the cursor to the top so the prompt prints properly
+  display.clear(); // Clear out whatever else might be on the screen
   display.print("Enter time~");
 }
 
 void KeypressHandler(int col, int row) {
- 
- wait_us(900000);
- newTime = 0;
- printf("Detected (%d,%d)\n", row, col);
+  wait_us(900000); // Wait until bounce has settled
+  newTime = 0; // Clear out whatever might be in newTime from the last keypress
   GPIOD->ODR |= (0x1 << 9);  // Turn on D9, the red LED
   wait_us(8000);             // Wait so the user can see the flash
   GPIOD->ODR &= ~(0x1 << 9); // Turn off D9, the red LED
@@ -215,13 +243,14 @@ void KeypressHandler(int col, int row) {
     } else if (col == 3) {
       enteringTime = false;
       timeRunning = true;
-      tick.attach(q1.event(timing), 1s);
-      timePos--;
+      tick.attach(q1.event(timing), 1s); // Trigger a call to timing every
+                                         // second
+      timePos--; // Since this isn't a number, don't increase the position in
+                 // the time entry
     }
-
   }
 
-if (row == 1) {
+  if (row == 1) {
     if (col == 0) {
       newTime = 4;
     } else if (col == 1) {
@@ -231,8 +260,9 @@ if (row == 1) {
     } else if (col == 3) {
       timeRunning = false;
       // User pressed B, so stop the timer
-      tick.detach();
-      timePos--;
+      tick.detach(); // Stop calling the timing function
+      timePos--; // Since this isn't a number, don't increase the position in
+                 // the time entry
     }
   }
 
@@ -246,7 +276,8 @@ if (row == 1) {
     } else if (col == 3) {
       // C is inoperative in normal use, but we should still detect it for the
       // bonus
-      timePos--;
+      timePos--; // Since this isn't a number, don't increase the position in
+                 // the time entry
     }
 
   }
@@ -257,9 +288,11 @@ if (row == 1) {
     }
 
     else if (col == 3) {
-      tick.detach();
+      // This is D, so user is entering time
+      tick.detach();             // Stop calling the timing function
       GPIOD->ODR &= ~(0x1 << 8); // Turn off D8, the white LED
       GPIOD->ODR &= ~(0x1 << 9); // Turn off D9, the red LED
+      // User wants to enter time, so clear all values from the previous entry
       firstDigit = 0;
       secondDigit = 0;
       thirdDigit = 0;
@@ -267,8 +300,9 @@ if (row == 1) {
       timePos = 0;
       enteringTime = true;
       timeRunning = false;
-      timePos--;
-      q1.call(timeEnter);
+      timePos--; // Since this isn't a number, don't increase the position in
+                 // the time entry
+      q1.call(timeEnter); // display the prompt
     }
   }
 
@@ -278,21 +312,25 @@ if (row == 1) {
            // the minutes place, and add 60 * that digit to the total time
     {
       firstDigit = newTime;
-      totalTime += newTime * 60;
-    } else if (timePos == 1 && newTime < 6) // If this is the second button the user pressed,
+      totalTime +=
+          newTime * 60; // The first digit is the minute, or 60-second place
+    } else if (timePos == 1 &&
+               newTime <
+                   6) // If this is the second button the user pressed,
                       // change the digit in the 10-second place and add 10
                       // * that digit to the total time if the new digit is
                       // less than 6 to avoid nonsense times like 2:65
     {
       secondDigit = newTime;
-      totalTime += newTime * 10;
+      totalTime += newTime * 10; // The second digit is the 10-second place
     } else {
       thirdDigit = newTime; // If this is the third button the user pressed,
                             // change the digit in the seconds place and add
                             // that digit to the total time
-      totalTime += newTime;
+      totalTime += newTime; // the third digit is the seconds place
     }
-    if (timePos < 3) // Times can only be 3 digits, so if the time is 2 or fewer,
+    if (timePos <
+        3) // Times can only be 3 digits, so if the time is 2 or fewer,
            // let the user edit the next digit with a button press
       timePos++;
     else {
@@ -308,20 +346,24 @@ void one(void) {
 }
 
 void two(void) {
-  KeypressHandler(1, row); // A key was pressed, so flash the LED
+  KeypressHandler(1, row); // A key was pressed, so let's figure out which one
+                           // and what to do about it
 }
 
 void three(void) {
-  KeypressHandler(2, row); // A key was pressed, so flash the LED
+  KeypressHandler(2, row); // A key was pressed, so let's figure out which one
+                           // and what to do about it
 }
 
 void A(void) {
-  KeypressHandler(3, row); // A key was pressed, so flash the LED
+  KeypressHandler(3, row); // A key was pressed, so let's figure out which one
+                           // and what to do about it
 }
 
 void displayTime() {
 
-  remainingTime[0] = '0' + firstDigit; //'0' is the character representation of the number 0
+  remainingTime[0] =
+      '0' + firstDigit; //'0' is the character representation of the number 0
                         // and since the ASCII values for numbers 0-9 are in
                         // order we can just add the digit to it
   remainingTime[1] = ':';
