0b040e86dd96e539fd23fae93919d7d82ed7faef
Skylar Kantor <skylkantor@gmail.com>
2021-11-30 16:19:23 -0500

    initial implementation of reading from DHT11 and displaying to sign

--- Project 3/cse321_alexkant_main.cpp
+++ Project 3/cse321_alexkant_main.cpp
@@ -1,28 +1,35 @@
-/**                                                                             
- *File: cse321_alexkant_project3_main.cpp                                       
- *Author Skylar Kantor (alexkant@buffalo.edu)                                   
- *Date: Fall 2021                                                               
- *Partner: None                                                                 
- *Course: CSE 321                                                               
- *Assignent: Project 3                                                         
- *Inputs: DHT11 Humidity sensor                                                                
- *Outputs: 7-segment display and LEDs                                                    
- *File Summary: Communicates with the DHT11 humidity and temperature sensor to find the dewpoint or whether the temperature is near freezing, calculates a safe speed limit, and displays that on the 7-segment display
- */  
+/**
+ *File: cse321_alexkant_project3_main.cpp
+ *Author Skylar Kantor (alexkant@buffalo.edu)
+ *Date: Fall 2021
+ *Partner: None
+ *Course: CSE 321
+ *Assignent: Project 3
+ *Inputs: DHT11 Humidity sensor
+ *Outputs: 7-segment display and LEDs
+ *File Summary: Communicates with the DHT11 humidity and temperature sensor to
+ *find the dewpoint or whether the temperature is near freezing, calculates a
+ *safe speed limit, and displays that on the 7-segment display
+ */
 
- #include "mbed.h"
-#include <cmath>
 #include "DHT.h"
 #include "DigitDisplay.h"
-
+#include "PinNames.h"
+#include "Watchdog.h"
+#include "mbed.h"
+#include <cmath>
 
 Thread readAndCalculate;
 Thread findSpeedLimitAndDisplay;
 
-bool is_dewy(int temp, int hum);
-int speed (int temp, int hum);
+void is_dewy(int temp, int hum);
+void speed(int temp, int hum);
 
-int postedLimit = 55;
+DHT11 sensor(PB_0);
+
+DigitDisplay sign(PA_0, PA_1);
+
+int *postedLimit;
 
 bool *atDewPoint;
 int *speedLimit;
@@ -31,43 +38,57 @@ int *humidity;
 
 Ticker tick;
 
-int main()
-{
-
-
-    return 0;
+int main() {
+  Watchdog::get_instance().start();
+  *postedLimit = 55;
+  sign.setColon(false);
+  sign.setBrightness(7);
+  return 0;
 }
 
-
-
-
-
-
-
-
-
-
-bool is_dewy(int temp, int hum)
-{
-    double dewPoint = temp-((100-hum)/5);
-    if(temp <= dewPoint)
-    return true;
-    else
-     return false;
+void is_dewy() {
+  double dewPoint = *(double *)temperature - ((100 - *(double *)humidity) / 5);
+  if (*temperature <= dewPoint)
+    *atDewPoint = true;
+  else
+    *atDewPoint = false;
+  Watchdog::get_instance().kick();
 }
 
-int speed(int temp, int hum, int *posted)
-{
-    double speed = *posted;
+void speed() {
+  double speed = *postedLimit;
 
-    //If the temperature is not near freezing (more than 5 degrees above 0C), and the road is not wet, keep the posted limit
-    if(!is_dewy(temp, hum) && temp >= 5)
-        return speed;
+  // If the temperature is not near freezing (more than 5 degrees above 0C), and
+  // the road is not wet, keep the posted limit Or, if there is an issue with the
+  // DHT 11, keep the posted limit
+  if ((!*atDewPoint && *temperature >= 5) || *humidity == -1)
+    *speedLimit = speed;
 
-    else if(is_dewy(temp, hum) && temp >= 5)
-        return round(speed/3);
-    else if(!is_dewy(temp, hum) && temp < 5)
-        return round(speed * 0.8);
-    else
-     return round(speed/8);
+  else if (*atDewPoint && *temperature >= 5)
+    *speedLimit = round(speed / 3);
+  else if (!*atDewPoint && *temperature < 5)
+    *speedLimit = round(speed * 0.8);
+  else
+    *speedLimit = round(speed / 8);
+  Watchdog::get_instance().kick();
+}
+
+void getTempAndHumidity() {
+  if (sensor.read() == 0) {
+    *temperature = sensor.getCelsius();
+    *humidity = sensor.getHumidity();
+  } else {
+    *temperature = 999;
+    *humidity = -1;
+  }
+  Watchdog::get_instance().kick();
+}
+
+void displaySpeedLimit() {
+  sign.clear();
+  sign.write(*speedLimit);
+  if (abs(*speedLimit - *postedLimit) > 5) {
+    // Flash connected LEDs
+  }
+  Watchdog::get_instance().kick();
 }
