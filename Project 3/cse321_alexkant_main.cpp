2ee52c1e9a3045c1346d8c558810fb34d20102c1
Skylar Kantor <skylkantor@gmail.com>
2021-11-30 15:23:01 -0500

    added header to main file

---
+++ Project 3/cse321_alexkant_main.cpp
@@ -0,0 +1,11 @@
+/**                                                                             
+ *File: cse321_alexkant_project3_main.cpp                                       
+ *Author Skylar Kantor (alexkant@buffalo.edu)                                   
+ *Date: Fall 2021                                                               
+ *Partner: None                                                                 
+ *Course: CSE 321                                                               
+ *Assignent: Project 3                                                         
+ *Inputs: DHT11 Humidity sensor                                                                
+ *Outputs: 7-segment display and LED      s                                                    
+ *File Summary: Communicates with the DHT11 humidity and temperature sensor to find the dewpoint or whether the temperature is near freezing, calculates a safe speed limit, and displays that on the 7-segment display
+ */  
